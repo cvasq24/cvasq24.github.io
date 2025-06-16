6a07af5abca94063b046ced4dd503528c6f5a542
cvasq24 <96681398+cvasq24@users.noreply.github.com>
2025-06-16 19:45:20 +0000

    Deploying to gh-pages from @ cvasq24/cvasq24.github.io@7ec4191d49b8f6a0a9a4ca0f673993c8cfcbd7e2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-test",
-    title: "Test",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
